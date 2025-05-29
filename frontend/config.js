0f38949ff3ab161347b0e59db7b0873a04fdd99d
wen952555 <wenxiu9525@outlook.com>
2025-05-29 10:01:50 +0800

    Delete frontend directory

--- frontend/config.js
+++
@@ -1,5 +0,0 @@
-// 配置文件：API与图片服务器URL
-const CONFIG = {
-  API_BASE_URL: "https://xxx.9525.ip-ddns.com/api", // 请替换为你的后端API地址
-  IMAGE_SERVER_BASE_URL: "https://xxx.9525.ip-ddns.com/assets/images/"
-};
