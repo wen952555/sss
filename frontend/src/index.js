b40cddf9cc7a93bf2b9b45a91b56f662368f3d2f
wen952555 <wenxiu9525@outlook.com>
2025-06-03 12:40:23 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // 适用于 React 18+
-// 如果你使用的是 React 17 或更早版本，请使用:
-// import ReactDOM from 'react-dom';
 import './index.css'; // 全局 CSS 文件 (可选)
 import App from './App'; // 你的主应用组件
-import reportWebVitals from './reportWebVitals'; // 用于性能监测 (可选)
+// import reportWebVitals from './reportWebVitals'; // <--- 移除或注释掉这一行
 
 // React 18+ 的方式
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,17 +12,7 @@ root.render(
   </React.StrictMode>
 );
 
-// 如果你使用的是 React 17 或更早版本，请取消注释以下代码并注释掉上面的 React 18+ 代码：
-/*
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-*/
-
 // 如果你想开始测量应用的性能，可以传递一个函数
 // 来记录结果 (例如: reportWebVitals(console.log))
 // 或者发送到一个分析端点。了解更多: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(); // <--- 移除或注释掉这一行
