1f19052bcf2687e4aae820c34e3b406ad4a15fc4
wen952555 <wenxiu9525@outlook.com>
2025-06-04 20:42:56 +0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css' // 如果你有全局样式文件
 import App from './App.vue'
-
 const app = createApp(App)
 app.use(createPinia())
 app.mount('#app')
