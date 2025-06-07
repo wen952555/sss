9e93e40eda0467fc93d7a356a10dc7dcb6eb06dd
wen952555 <wenxiu9525@outlook.com>
2025-06-07 10:12:09 +0800

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import GameBoard from './components/GameBoard';
+
+function App() {
+  return (
+    <div className="app-container">
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
