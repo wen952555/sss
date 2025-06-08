cc852386084410ebdcb48340bcf142a9f58bf8ef
wen952555 <wenxiu9525@outlook.com>
2025-06-08 09:46:36 +0800

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GameTable from './components/GameTable';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GameTable />
+    </div>
+  );
+}
+
+export default App;
