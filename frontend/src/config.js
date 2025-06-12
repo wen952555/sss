1059e292f081d146437075d001c5601ec8169609
wen952555 <wenxiu9525@outlook.com>
2025-06-12 22:48:44 +0800

    Create config.js

---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+// frontend/src/config.js
+export const API_BASE_URL = 'https://wenge.cloudns.ch:10758'; // Base URL for backend
+export const POKER_IMAGE_PATH = '/poker_images/'; // Path in public folder
