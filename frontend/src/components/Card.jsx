9a8f8feb89314abc5ac072eb9c768cbe0aed6af4
wen952555 <wenxiu9525@outlook.com>
2025-06-15 14:05:28 +0800

    Update Card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,33 +1,21 @@
-import React from 'react';
-
-const Card = ({ card, onClick, isSelected, isPlayed }) => {
-  if (!card) return null;
+// 更新图片路径处理
+const getCardImage = () => {
+  const { suit, value } = card;
+  let valueName;
   
-  const getCardImage = () => {
-    const { suit, value } = card;
-    let valueName;
-    
-    if (value === 1) valueName = 'ace';
-    else if (value === 11) valueName = 'jack';
-    else if (value === 12) valueName = 'queen';
-    else if (value === 13) valueName = 'king';
-    else valueName = value.toString();
-    
-    return `${valueName}_of_${suit}`;
-  };
-
-  return (
-    <div 
-      className={`card ${isSelected ? 'selected' : ''} ${isPlayed ? 'played' : ''}`}
-      onClick={onClick}
-    >
-      <img 
-        src={`/images/cards/${getCardImage()}.svg`} 
-        alt={`${card.value} of ${card.suit}`}
-        className="card-image"
-      />
-    </div>
-  );
+  if (value === 1) valueName = 'ace';
+  else if (value === 11) valueName = 'jack';
+  else if (value === 12) valueName = 'queen';
+  else if (value === 13) valueName = 'king';
+  else valueName = value.toString();
+  
+  // 确保路径格式正确
+  return `${valueName}_of_${suit}`;
 };
 
-export default Card;
+// 在img标签中使用
+<img 
+  src={`${process.env.PUBLIC_URL}/images/cards/${getCardImage()}.svg`}
+  alt={`${card.value} of ${card.suit}`}
+  className="card-image"
+/>
