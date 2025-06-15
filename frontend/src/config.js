290882152923eaed2f7a18dee88e3be69842f432
wen952555 <wenxiu9525@outlook.com>
2025-06-15 13:43:05 +0800

    Delete frontend directory

--- frontend/src/config.js
+++
@@ -1,9 +0,0 @@
-// frontend/src/config.js
-
-// 旧的配置:
-// export const API_BASE_URL = 'https://wenge.cloudns.ch:10758';
-// export const POKER_IMAGE_PATH = '/poker_images/';
-
-// 新的配置:
-export const API_BASE_URL = 'https://9525.ip-ddns.com/backend'; // 更新后端域名和端口
-export const POKER_IMAGE_PATH = '/poker_images/'; // 这个通常不需要改，除非你改变了public文件夹结构
