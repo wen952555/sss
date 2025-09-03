febcfeaed1de194533666c9b69a16453021e8b7f
wen952555 <wenxiu9525@outlook.com>
2025-09-03 15:40:45 +0800

    Update game.php

--- backend/api/game.php
+++ backend/api/game.php
@@ -6,10 +6,7 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // 创建一副牌
-function createDeck() {
-    $suits = ['hearts', 'diamonds',
-        foreach ($values as $va
-    shuffle($deck);
+function createD
     return $deck;
 }
 
