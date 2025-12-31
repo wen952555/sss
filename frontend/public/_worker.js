15998a0305710be4641d8ad1a94839ed40ebf6e3
wenxiu775866 <wenxiu775866@gmail.com>
2025-12-31 12:00:21 +0000

    fix: forward query params in worker

--- frontend/public/_worker.js
+++ frontend/public/_worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     if (url.pathname.startsWith('/api/')) {
-      const backendUrl = 'https://wen76674.serv00.net' + url.pathname.replace('/api', '');
+      const backendUrl = 'https://wen76674.serv00.net' + url.pathname.replace('/api', '') + url.search;
       
       const newRequest = new Request(backendUrl, {
         method: request.method,
@@ -16,7 +16,8 @@ export default {
       // 克隆响应以修改头部
       const newResponse = new Response(response.body, response);
       newResponse.headers.set('Access-Control-Allow-Origin', url.origin);
-      newResponse.headers.set('Access-Control-Allow-Credentials', 'true');
+      newResponse.headers.set('Access-Control-Allow-Credentials', 'true
+      ');
       newResponse.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
       
       return newResponse;
