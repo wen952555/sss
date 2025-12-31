a51993167e38476bb664b77fbc966e2b8ec3a480
wenxiu775866 <wenxiu775866@gmail.com>
2025-12-31 12:01:26 +0000

    fix: correct syntax error in worker

--- frontend/public/_worker.js
+++ frontend/public/_worker.js
@@ -16,8 +16,7 @@ export default {
       // 克隆响应以修改头部
       const newResponse = new Response(response.body, response);
       newResponse.headers.set('Access-Control-Allow-Origin', url.origin);
-      newResponse.headers.set('Access-Control-Allow-Credentials', 'true
-      ');
+      newResponse.headers.set('Access-Control-Allow-Credentials', 'true');
       newResponse.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
       
       return newResponse;
